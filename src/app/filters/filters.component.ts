18cf676f8db0bc87fb042c90b246c75e808c7a48
JP <jp@beyondlogical.com>
2019-10-17 13:24:41 -0400

    added toys

--- src/app/filters/filters.component.ts
+++ src/app/filters/filters.component.ts
@@ -13,7 +13,7 @@ export class Item{
     this.mfg = mfg;
   }
 }
-//security catergory, layer
+//security category, layer
 @Component({
   selector: 'app-filters',
   templateUrl: './filters.component.html',
@@ -21,16 +21,20 @@ export class Item{
 })
 
 export class FiltersComponent implements OnInit {
-  foods:Array<Item>;
+  foods: Array<Item> = [];
+  toys: Array<Item> = [];
   categories: Array<String>;
   constructor() {
     this.categories = ['Food', 'Toys'];
-    this.foods = [];
     let food1 = new Item('solid','dinner', 1.75, 'purina', 'food');
-    let food2 = new Item('wet','chicken & rice', 1.75, 'purina', 'food');
-    
-    Array.prototype.push.apply(this.foods, [food1,food2])
+    let food2 = new Item('wet','chicken & rice', 2.39, 'purina', 'food');
+    let toy1 = new Item('plush','Happy Emoji', 5.75, 'toyco', 'toy');
+    let toy2 = new Item('electronic','laser pointer', 9.95, 'laserco', 'toy');
+    let toy3 = new Item('game','feather teather', 3.95, 'tyco', 'toy');
+    Array.prototype.push.apply(this.foods, [food1,food2]);
+    Array.prototype.push.apply(this.toys, [toy1,toy2, toy3]);
     console.log(this.foods);
+    console.log(this.toys);
   }
 
   ngOnInit() {
