4939d590625196ac3040d91f140bf95da71453ea
JP <jp@beyondlogical.com>
2019-10-18 10:13:00 -0400

    added ids to json database

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -5,11 +5,11 @@ import { Injectable } from '@angular/core';
 })
 export class DataService {
   myJson = [
-    {'type':'solid', 'name': 'quail & pumpkin', 'price':1.75, 'mfg': 'Solid Gold', 'category': 'food', 'active' : true }
-    , {'type':'wet', 'name': 'chicken & rice', 'price':2.39, 'mfg': 'Purina', 'category': 'food', 'active' : true }
-    , {'type':'plushie', 'name': 'Happy Emoji', 'price':5.75, 'mfg': 'toyco', 'category': 'toy', 'active' : true }
-    , {'type':'electronic', 'name': 'laser pointer', 'price':9.95, 'mfg': 'laserco', 'category': 'toy', 'active' : true }
-    , {'type':'game', 'name': 'Feather Teather', 'price':1.75, 'mfg': 'tyco', 'category': 'toy', 'active' : true }
+    {'id':1,'type':'solid', 'name': 'quail & pumpkin', 'price':1.75, 'mfg': 'Solid Gold', 'category': 'food', 'active' : true }
+    , {'id':2,'type':'wet', 'name': 'chicken & rice', 'price':2.39, 'mfg': 'Purina', 'category': 'food', 'active' : true }
+    , {'id':3,'type':'plushie', 'name': 'Happy Emoji', 'price':5.75, 'mfg': 'toyco', 'category': 'toy', 'active' : true }
+    , {'id':4,'type':'electronic', 'name': 'laser pointer', 'price':9.95, 'mfg': 'laserco', 'category': 'toy', 'active' : true }
+    , {'id':5,'type':'game', 'name': 'Feather Teather', 'price':1.75, 'mfg': 'tyco', 'category': 'toy', 'active' : true }
   ]
   constructor() {
    }
